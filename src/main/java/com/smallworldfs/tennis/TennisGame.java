13e8d962629b04d902ff93a23aecb0ca90c186d4
Lydia Ramos <lydia.ramos@smallworldfs.com>
2022-11-25 12:06:51 +0100

    WIP: refactoring

--- src/main/java/com/smallworldfs/tennis/TennisGame.java
+++ src/main/java/com/smallworldfs/tennis/TennisGame.java
@@ -1,8 +1,9 @@
 package com.smallworldfs.tennis;
 
-public class TennisGame
+import java.util.HashMap;
+import java.util.Map;
 
-{
+public class TennisGame{
 
     public int P1point = 0;
     public int P2point = 0;
@@ -11,70 +12,46 @@ public class TennisGame
     public String P2res = "";
     private String player1Name;
     private String player2Name;
+    public Map<Integer, String> pointsTranslations = new HashMap<Integer, String>();
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        loadPointsTranslations();
+    }
+
+    private void loadPointsTranslations() {
+        pointsTranslations.put(0, "Love");
+        pointsTranslations.put(1, "Fifteen");
+        pointsTranslations.put(2, "Thirty");
+        pointsTranslations.put(3, "Forty");
     }
 
     public String getScore() {
         String score = "";
         if (P1point == P2point && P1point < 4) {
-            if (P1point == 0)
-                score = "Love";
-            if (P1point == 1)
-                score = "Fifteen";
-            if (P1point == 2)
-                score = "Thirty";
+            if (P1point == 0 || P1point == 1 || P1point == 2)
+                score = pointsTranslations.get(P1point);
             score += "-All";
         }
+
         if (P1point == P2point && P1point >= 3)
             score = "Deuce";
 
         if (P1point > 0 && P2point == 0) {
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-
-            P2res = "Love";
-            score = P1res + "-" + P2res;
+            score = onePlayerScoresTheOtherDoesnt(score, P1point, P2point);
         }
-        if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
 
-            P1res = "Love";
-            score = P1res + "-" + P2res;
+        if (P2point > 0 && P1point == 0) {
+            score = onePlayerScoresTheOtherDoesnt(score, P2point, P1point);
         }
 
         if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            score = P1res + "-" + P2res;
+            score = onePlayerIsWinning(score, P1point, P2point);
         }
+
         if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            score = P1res + "-" + P2res;
+            score = onePlayerIsWinning(score, P2point, P1point);
         }
 
         if (P1point > P2point && P2point >= 3) {
@@ -92,6 +69,33 @@ public class TennisGame
             score = "Win for player2";
         }
         return score;
+    }
+
+    private String onePlayerScoresTheOtherDoesnt(String score, int PScorer, int PNonScorer) {
+        if (PScorer == 1 || PScorer == 2 || PScorer == 3)
+            fillPlayerPoints(PScorer == P1point, pointsTranslations.get(PScorer));
+
+        fillPlayerPoints(PScorer != P1point, "Love");
+        score = P1res + "-" + P2res;
+
+        return score;
+    }
+
+    private String onePlayerIsWinning(String score, int PScorer, int PNonScorer) {
+        if (PScorer == 2 || PScorer == 3)
+            fillPlayerPoints(PScorer == P1point, pointsTranslations.get(PScorer));
+        if (PNonScorer == 1 || PNonScorer == 2)
+            fillPlayerPoints(PScorer != P1point, pointsTranslations.get(PNonScorer));
+        score = P1res + "-" + P2res;
+        return score;
+    }
+
+    private void fillPlayerPoints(boolean fillP1, String value) {
+        if (fillP1) {
+            P1res = value;
+        } else {
+            P2res = value;
+        }
     }
 
     public void SetP1Score(int number) {
